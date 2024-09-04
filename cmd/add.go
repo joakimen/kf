92f3f55ced1e5246446dd52a9de524cec678c3e3
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-09-04 12:15:42 +0200

    refactor: remove unnecessary var block

--- cmd/add.go
+++ cmd/add.go
@@ -8,32 +8,30 @@ import (
 	"os"
 )
 
-var (
-	addCmd = &cobra.Command{
-		Use:   "add",
-		Short: "Add a file to the list of known files",
-		Args:  cobra.MatchAll(cobra.ExactArgs(1)),
-		Run: func(cmd *cobra.Command, args []string) {
-			fileToAddAbs, err := fs.RealPath(args[0])
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error getting real path: %v\n", err)
-				os.Exit(1)
-			}
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a file to the list of known files",
+	Args:  cobra.MatchAll(cobra.ExactArgs(1)),
+	Run: func(cmd *cobra.Command, args []string) {
+		fileToAddAbs, err := fs.RealPath(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error getting real path: %v\n", err)
+			os.Exit(1)
+		}
 
-			if !fs.IsValidFile(fileToAddAbs) {
-				fmt.Fprintf(os.Stderr, "not a valid file: %s\n", fileToAddAbs)
-				os.Exit(1)
-			}
+		if !fs.IsValidFile(fileToAddAbs) {
+			fmt.Fprintf(os.Stderr, "not a valid file: %s\n", fileToAddAbs)
+			os.Exit(1)
+		}
 
-			err = config.AppendToConfigFile(fileToAddAbs)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error appending to configuration file: %v\n", err)
-				os.Exit(1)
-			}
-			fmt.Println("kf: added", args[0])
-		},
-	}
-)
+		err = config.AddEntry(fileToAddAbs)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error appending to configuration file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("kf: added", args[0])
+	},
+}
 
 func init() {
 	rootCmd.AddCommand(addCmd)
